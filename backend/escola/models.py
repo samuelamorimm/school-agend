b987a857eca32ada4008832a07500efb44f32191
samuelamorimm <samuelhenriqueamorim@gmail.com>
2025-05-10 19:23:57 -0300

    finalizando models

--- backend/escola/models.py
+++ backend/escola/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager, User
 
 # Create your models here.
 class UserManager(BaseUserManager):
@@ -26,8 +26,6 @@ class UserManager(BaseUserManager):
     return self.create_user(username, email, password, **extra_fields)
     
 
-
-
 class CustomUser(AbstractUser):
   OCUPPATION_CHOICES = [
     ('ADM', 'Administrador'),
@@ -36,9 +34,31 @@ class CustomUser(AbstractUser):
 
   name = models.CharField(max_length=200, blank=False) 
   phone = models.CharField(max_length=11, blank=False)
-  occupation = models.CharField(max_length=3, choices=OCUPPATION_CHOICES, blank=False)
+  occupation = models.CharField(max_length=3, choices=OCUPPATION_CHOICES, default='PRO', blank=False)
 
   objects = UserManager()
 
   def __str__(self):
     return self.username
+  
+
+class Classroom(models.Model):
+  name = models.CharField(max_length=200, blank=False)
+  professor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=False)
+
+class Meeting(models.Model):
+  STATUS_CHOICES = [
+    ('F', 'Finalizada'),
+    ('A', 'Em andamento'),
+    ('B', 'Em breve'),
+  ]
+
+  title = models.CharField(max_length=200)
+  description = models.TextField(blank=True)
+  classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+  professor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=False)
+  start_time = models.DateTimeField(blank=False)
+  end_time = models.DateTimeField(blank=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  status = models.CharField(max_length=1, default='I', blank=False)
+
